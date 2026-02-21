72dc672fa08d15dcff5cc55f2456cade3513e781
Adam <156256297+sadam-04@users.noreply.github.com>
2026-02-21 14:35:42 -0500

    remove absolute paths

--- forge.config.js
+++ forge.config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       name: '@electron-forge/maker-squirrel',
       config: {
-        setupIcon: 'C:/repos/Axon-node/icon/axon.ico',
+        setupIcon: 'icon/axon.ico',
         
       },
     },
