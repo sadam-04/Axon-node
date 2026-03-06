c8f72667cdfcf5815dbbe60045ca64c8cbd815f6
Adam <156256297+sadam-04@users.noreply.github.com>
2026-03-06 10:14:40 -0500

    Revert "add qr button shade params + show QR button in inbox"
    This reverts commit 189df28fc685796bb36ac7460c79719ce72c2db9.

--- src/components/QrComponent.jsx
+++ src/components/QrComponent.jsx
@@ -1,9 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import QRCode from 'qrcode';
 
-import ResponsiveButton from './ResponsiveButton';
-
-const QrComponent = ({url, alignment, shadeA="#282828", shadeB="#303030", shadeC="#383838"}) => {
+const QrComponent = ({url}) => {
   const [src, setSrc] = useState("");
   const [qrHoverMsg, setQrHoverMsg] = useState("Click to copy URL to clipboard");
 
@@ -28,14 +26,9 @@ const QrComponent = ({url, alignment, shadeA="#282828", shadeB="#303030", shadeC
   }
 
   return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center", gap: "10px", fontSize: "0.8rem"}}>
-      <div className="qr-code" style={{position: "relative", width: "fit-content", height: "fit-content", aspectRatio: "1/1", textAlign: "center", fontStyle: "italic", cursor: "pointer", borderRadius: "8px", whiteSpace: "normal"}} onClick={handleQRClick} onMouseLeave={() => setQrHoverMsg("Click to copy URL to clipboard")}>
-        {src ? <img src={src} /> : <p>Loading QR...</p>}
-        <div className="qr-overlay"><div className="qr-overlay-text">{qrHoverMsg}</div></div>
-      </div>
-
-      {showUrl ? (<div style={{alignSelf: {alignment}}}>{url.toLowerCase()}</div>) : null}
-      <ResponsiveButton selected={false} enabled={true} buttonAction={()=>{setShowUrl(!showUrl)}} label={showUrl ? "Hide" : "Show"} customStyle={{borderRadius: "5px", width: "fit-content", paddingLeft: "10px", paddingRight: "10px", height: "30px"}} shadeA={shadeA} shadeB={shadeB} shadeC={shadeC} />
+    <div className="qr-wrapper" onClick={handleQRClick} onMouseLeave={() => setQrHoverMsg("Click to copy URL to clipboard")}>
+      {src ? <img src={src} /> : <p>Loading QR...</p>}
+      <div className="qr-overlay"><div className="qr-overlay-text">{qrHoverMsg}</div></div>
     </div>
   );
 }
