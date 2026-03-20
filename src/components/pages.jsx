3aa7a9e69a7f77ec45a71752b040670e3248f16f
Adam <156256297+sadam-04@users.noreply.github.com>
2026-03-20 15:04:00 -0400

    incremental scrollbar

--- src/components/pages.jsx
+++ src/components/pages.jsx
@@ -12,8 +12,8 @@ const Outbox = ({hostedFiles, setHostedFiles, openFile, handleAddText, addTextVa
 
     return (
         <div style={{display: "flex", flexDirection: "row", height: "100%", width: "100%"}}>
-            <div id="left-summary-panel" style={{display: "flex", flexDirection: "column"}}>
-                <div>
+            <div id="left-summary-panel" style={{display: "flex", flexDirection: "column", boxSizing: "content-box"}}>
+                <div style={{maxHeight: "100%"}}>
                     <div className="left-send-header" onClick={() => {setSelectedSFile(null);}} style={{ display: "flex", justifyItems: "space-between", flexDirection: "column", marginBottom: "0"}}>
                         <h4 style={{marginBottom: "5px", marginTop: "5px", marginLeft: "12px"}}>Outbox</h4>
                         <div style={{display: "flex", flexDirection: "row", alignItems: "space-between", height: "25px"}}>
@@ -35,7 +35,7 @@ const Outbox = ({hostedFiles, setHostedFiles, openFile, handleAddText, addTextVa
                         shadeC={"#343434"}
                         />
                     </div>
-                    <div style={{marginLeft: "0", marginRight: "0", marginTop: "6px"}}>
+                    <div style={{marginLeft: "0", marginRight: "0", marginTop: "6px", display: "block", overflowY: "auto"}}>
                     {hostedFiles.map((file, i) => (
                         <ResponsiveButton
                         key={file.id}
